8206d8888cb10e3a8848e84c53cba6f913c1be98
unkuz <hi.unkuz@gmail.com>
2024-02-08 11:23:52 +0700

    🙄

--- server.ts
+++ server.ts
@@ -1,8 +1,8 @@
-import "module-alias/register";
-import { app } from "@@/app";
+import 'module-alias/register'
+import { app } from '@@/app'
 
-const PORT = 8888;
+const PORT = 8888
 
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-});
+  console.log(`App listening on port ${PORT}`)
+})
