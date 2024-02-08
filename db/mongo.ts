56bf3b329418807c335e974f89e2e4b59f332ea8
unkuz <hi.unkuz@gmail.com>
2024-02-08 17:15:43 +0700

    😂

--- db/mongo.ts
+++ db/mongo.ts
@@ -2,18 +2,6 @@ import mongoose from 'mongoose'
 
 const connectStr = `mongodb://localhost:27017/miracle`
 
-export const connectMongo = async () => {
-  try {
-    await mongoose.connect(connectStr)
-    console.log(`Connect  MongoDB success`)
-  } catch (e: any) {
-    console.log(`Connect mongoBD fail: ${e?.message}`)
-  } finally {
-    mongoose.set('debug', true)
-    mongoose.set('debug', { color: true })
-  }
-}
-
 class MongoDB {
   private static instance: MongoDB | null = null
   public connection: undefined
