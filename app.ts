8eb9c616443ce1818dba0790bd81be08df41f512
unkuz <hi.unkuz@gmail.com>
2024-02-08 17:15:20 +0700

    🙄

--- app.ts
+++ app.ts
@@ -3,10 +3,13 @@ import morgan from 'morgan'
 import helmet from 'helmet'
 import cors from 'cors'
 import compression from 'compression'
+import { connectMongoDB } from './db/mongo'
 
 export const app = express()
 
-app.use(morgan('tiny'))
+app.use(morgan('combined'))
 app.use(helmet())
 app.use(cors())
 app.use(compression())
+
+connectMongoDB()
---
+++ db/mongo.ts
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose'
+
+const connectStr = `mongodb://localhost:27017/miracle`
+
+export const connectMongo = async () => {
+  try {
+    await mongoose.connect(connectStr)
+    console.log(`Connect  MongoDB success`)
+  } catch (e: any) {
+    console.log(`Connect mongoBD fail: ${e?.message}`)
+  } finally {
+    mongoose.set('debug', true)
+    mongoose.set('debug', { color: true })
+  }
+}
+
+class MongoDB {
+  private static instance: MongoDB | null = null
+  public connection: undefined
+
+  constructor() {
+    this.connect()
+  }
+
+  async connect() {
+    mongoose.set('debug', true)
+    mongoose.set('debug', { color: true })
+    try {
+      await mongoose.connect(connectStr)
+      console.log(`Connect  MongoDB success`)
+    } catch (e: any) {
+      console.log(`Connect mongoBD fail: ${e?.message}`)
+    } finally {
+    }
+  }
+
+  static getInstance() {
+    !MongoDB.instance && (MongoDB.instance = new MongoDB())
+    return MongoDB.instance
+  }
+}
+
+export const connectMongoDB = MongoDB.getInstance
