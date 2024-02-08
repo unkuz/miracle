c49efa80494c0d310a97ec91a5744ba674a073d0
unkuz <hi.unkuz@gmail.com>
2024-02-08 11:15:50 +0700

    😂

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cors from "cors";
+import compression from "compression";
+
+export const app = express();
+
+app.use(morgan("tiny"));
+app.use(helmet());
+app.use(cors());
+app.use(compression());
